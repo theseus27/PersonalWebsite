082bde7b5c621a4fb78a0856c8673ae00b8f21b0
theseus27 <theseus.lim@gmail.com>
2024-04-03 14:03:40 -0400

    react rm new index page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          App.js from src folder
         </a>
       </header>
     </div>
